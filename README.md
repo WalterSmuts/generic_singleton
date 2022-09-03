ab45cf5e0c63bbce7b99548817c31c8fdd6d8d1e
Walter Smuts <smuts.walter@gmail.com>
2022-09-03 17:56:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# generic\_singleton
+Rust does NOT monomorphize it's [static generic items]. This is dissappointing when you have a
+generic function that contains a static variable depending on that generic. You'll get the
+following error:
+```text
+error[E0401]: can't use generic parameters from outer function
+```
+
+That's pretty frustrating when you want to write a singleton pattern that rely's on a generic
+parameter. That's where this crate saves the day!
+
+`generic_singleton` uses [anymap] behind the scenes to store a map of each generic type. The first
+time you call `get_or_init` we initialize the singleton in thread local storage. Subsequent
+calls to `get_or_init` will retrieve the singleton from the map.
+
+### Example
+```rust
+use std::sync::Mutex;
+
+fn generic_function<T: Copy + std::ops::Add<Output = T> + 'static>(initializer: fn() -> Mutex<T>) -> T {
+    {
+        let mut a = generic_singleton::get_or_init(initializer).lock().unwrap();
+        let b = *a;
+        *a = *a + b;
+        *a
+    }
+}
+
+fn main() {
+    assert_eq!(generic_function(||Mutex::new(2)), 4);
+    assert_eq!(generic_function(||Mutex::new(2)), 8);
+    assert_eq!(generic_function(||Mutex::new(2)), 16);
+
+    assert_eq!(generic_function(||Mutex::new(2.0)), 4.0);
+    assert_eq!(generic_function(||Mutex::new(2.0)), 8.0);
+    assert_eq!(generic_function(||Mutex::new(2.0)), 16.0);
+}
+```
+
+### Current limitations:
+- Each thread has a different map. I may add `get_or_init_local` and `get_or_init_global` if I
+can figure out how to implement concurrent access and the required trait bound to make it safe.
+- There is only one map(per thread). Mutliple uses of `get_or_init` on the same type will
+return the __SAME__ singleton. Perhaps even accross crate boundaries. I might be able to limit
+it by providing a macro that salts the key in `AnyMap` with a context about which function it's
+being called from.
+
+[static generic items]: https://doc.rust-lang.org/reference/items/static-items.html#statics--generics
+[anymap]: https://docs.rs/anymap/latest/anymap/
---
+++ src/lib.rs
@@ -0,0 +1,68 @@
+#![deny(missing_docs)]
+#![deny(clippy::undocumented_unsafe_blocks)]
+#![doc = include_str!("../README.md")]
+
+use anymap::AnyMap;
+use std::cell::RefCell;
+
+/// Get a static reference to a generic singleton or initialize it if it doesn't exist.
+///
+/// # Panics
+/// Initialization will panic if the init function calls get_or_init during initialization.
+pub fn get_or_init<T: 'static>(init: fn() -> T) -> &'static T {
+    // TODO: Consider using UnsafeCell to avoid runtime borrow-checking. As it stands right now
+    //       it's probably not a good idea since the user will be able to trigger UB in the case
+    //       where currently the code would panic. If we can remove the panic we can most likely
+    //       safely switch to using UnsafeCell.
+    //
+    thread_local! {
+        static REF_CELL_MAP: RefCell<AnyMap> = RefCell::new(AnyMap::new());
+    };
+    REF_CELL_MAP.with(|map_cell| {
+        let mut map = map_cell.borrow_mut();
+        if !map.contains::<T>() {
+            map.insert(init());
+        }
+        // SAFETY:
+        // The function will only return None if the item is not present. Since we always add the
+        // item if it's not present two lines above and never remove items, we can be sure that
+        // this function will always return `Some`.
+        let t_ref = unsafe { map.get::<T>().unwrap_unchecked() };
+        let ptr = t_ref as *const T;
+        // SAFETY:
+        // Check: The pointer must be properly aligned.
+        // Proof: The pointer is obtained from a valid reference so it must be aligned.
+        //
+        // Check: It must be “dereferenceable” in the sense defined in the module documentation.
+        // Proof: The pointer is obtained from a valid reference it musts be dereferenceable.
+        //
+        // Check: The pointer must point to an initialized instance of T.
+        // Proof: The AnyMap crate provides this guarantee.
+        //
+        // Check: You must enforce Rust’s aliasing rules, since the returned lifetime 'a is
+        //        arbitrarily chosen and does not necessarily reflect the actual lifetime of the data.
+        //        In particular, while this reference exists, the memory the pointer points to
+        //        must not get mutated (except inside UnsafeCell).
+        // Proof: We return a shared reference and therefore cannot be mutated unless T is
+        //        guarded with the normal rust memory protection constructs using UnsafeCell.
+        //        The data could be dropped if we ever removed it from this map however. Care
+        //        must be taken to never introduce any logic that would remove T from the map.
+        let optional_ref = unsafe { ptr.as_ref() };
+        // SAFETY:
+        // This requires the pointer not to be null. We obtained the pointer one line above
+        // from a valid reference, therefore this is considered safe to do.
+        unsafe { optional_ref.unwrap_unchecked() }
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn works() {
+        let a = get_or_init(|| 0);
+        let b = get_or_init(|| 1);
+        assert_eq!(a, b);
+    }
+}
