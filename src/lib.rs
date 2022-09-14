d71d322355dce0cbb6671c6c7b2e21af2732966a
Walter Smuts <smuts.walter@gmail.com>
2022-09-14 09:40:25 +0200

    Rework using static pinned memory and RwLock
    This involves creating StaticAnyMap struct that maintains the pinned and
    no-removing invariants. This allows for safely returning 'static
    references.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 #![deny(missing_docs)]
 #![deny(clippy::undocumented_unsafe_blocks)]
 #![doc = include_str!("../README.md")]
-use anymap::AnyMap;
-use std::cell::UnsafeCell;
 pub extern crate anymap;
+
+#[doc(hidden)]
+pub mod static_anymap;
 
 /// Get a static reference to a generic singleton or initialize it if it doesn't exist.
 ///
@@ -54,71 +55,13 @@ pub extern crate anymap;
 #[macro_export]
 macro_rules! get_or_init {
     ($init:expr) => {{
-        use std::cell::UnsafeCell;
-        use $crate::anymap::AnyMap;
-        use $crate::get_from_map;
+        use $crate::static_anymap::StaticAnyMap;
 
-        thread_local! {
-            static UNSAFE_CELL_MAP: UnsafeCell<AnyMap> = UnsafeCell::new(AnyMap::new());
-        };
-        UNSAFE_CELL_MAP.with(|map_cell| get_from_map(map_cell, $init))
+        lazy_static::lazy_static! {
+            static ref STATIC_ANY_MAP: StaticAnyMap = StaticAnyMap::default();
+        }
+        STATIC_ANY_MAP.get_or_init($init)
     }};
-}
-
-/// Used by the [`get_or_init`] macro to access the generic item from the map. Do not use
-/// directly. Use the [`get_or_init`] macro instead.
-pub fn get_from_map<T>(map_cell: &UnsafeCell<AnyMap>, init: impl FnOnce() -> T) -> &'static T {
-    // Curly brackets are important here to drop the borrow after checking
-    let contains = {
-        // SAFETY:
-        // This was tested using a RefCell in the following commit:
-        // d23f9f28ed6b9d1b61cc82e9e2cff17c7f4575c1
-        let map = unsafe { map_cell.get().as_ref().unwrap() };
-        map.contains::<T>()
-    };
-    if !contains {
-        // Important to calculate this value BEFORE we borrow the map
-        let val = init();
-        // SAFETY:
-        // This was tested using a RefCell in the following commit:
-        // d23f9f28ed6b9d1b61cc82e9e2cff17c7f4575c1
-        let map = unsafe { map_cell.get().as_mut().unwrap_unchecked() };
-        map.insert(val);
-        // Drop the borrow
-    }
-    // SAFETY:
-    // This was tested using a RefCell in the following commit:
-    // d23f9f28ed6b9d1b61cc82e9e2cff17c7f4575c1
-    let map = unsafe { map_cell.get().as_ref().unwrap() };
-    // SAFETY:
-    // The function will only return None if the item is not present. Since we always add the
-    // item if it's not present two lines above and never remove items, we can be sure that
-    // this function will always return `Some`.
-    let t_ref = unsafe { map.get::<T>().unwrap_unchecked() };
-    let ptr = t_ref as *const T;
-    // SAFETY:
-    // Check: The pointer must be properly aligned.
-    // Proof: The pointer is obtained from a valid reference so it must be aligned.
-    //
-    // Check: It must be “dereferenceable” in the sense defined in the module documentation.
-    // Proof: The pointer is obtained from a valid reference it musts be dereferenceable.
-    //
-    // Check: The pointer must point to an initialized instance of T.
-    // Proof: The AnyMap crate provides this guarantee.
-    //
-    // Check: You must enforce Rust’s aliasing rules, since the returned lifetime 'a is
-    //        arbitrarily chosen and does not necessarily reflect the actual lifetime of the data.
-    //        In particular, while this reference exists, the memory the pointer points to
-    //        must not get mutated (except inside UnsafeCell).
-    // Proof: We return a shared reference and therefore cannot be mutated unless T is
-    //        guarded with the normal rust memory protection constructs using UnsafeCell.
-    //        The data could be dropped if we ever removed it from this map however. Care
-    //        must be taken to never introduce any logic that would remove T from the map.
-    let optional_ref = unsafe { ptr.as_ref() };
-    // SAFETY:
-    // This requires the pointer not to be null. We obtained the pointer one line above
-    // from a valid reference, therefore this is considered safe to do.
-    unsafe { optional_ref.unwrap_unchecked() }
 }
 
 #[cfg(test)]
@@ -139,5 +82,14 @@ mod tests {
     #[test]
     fn recursive_call_to_get_or_init_does_not_panic() {
         get_or_init!(|| get_or_init!(|| 0));
+    }
+
+    #[test]
+    fn initializing_in_different_thread_works() {
+        let v = std::thread::spawn(|| get_or_init!(|| String::from("foo")))
+            .join()
+            .unwrap();
+
+        println!("{v}"); // Check for use-after-free, to be sure bugs are caught, run with miri
     }
 }
---
+++ src/static_anymap.rs
@@ -0,0 +1,107 @@
+use std::pin::Pin;
+
+use anymap::AnyMap;
+use parking_lot::RwLock;
+
+/// The point of this struct is to wrap the AnyMap in a concurrent, static version that will only
+/// insert items that have 'static lifetimes. This is acieved by wrapping all items in Pin<Box<T>>
+/// and never removing items. This module only exposes the StaticAnyMap struct and it's get_or_init
+/// method. Using these should be perfectly safe.
+#[derive(Default)]
+pub struct StaticAnyMap {
+    inner: RwLock<AnyMap>,
+}
+
+// SAFETY:
+// From the rustonomicon:
+// https://doc.rust-lang.org/nomicon/send-and-sync.html
+// "A type is Sync if it is safe to share between threads (T is Sync if and only if &T is Send)."
+// All accessors are only implemented for 'static references, so I believe we can say &T ('static
+// implied) is Send. And since any mutation is guarded by a RwLock, the type should be sync too.
+//
+// I'm a bit uncertain about the above. I've managed to limit access to the inner AnyMap via
+// 'static references, but there's nothing stopping a user from creating a non-'static
+// StaticAnyMap. The impl below allows to send it between threads, even if the type is not
+// 'static. This instance would be unusable but perhaps it can still trigger UB. For example in
+// the following podcast at ~14:10, Andrew Kelly mentions the existence of a dangling pointer
+// might cause undefined behaviour:
+// https://rustacean-station.org/episode/andrew-kelley/
+// I'm concerned that, similar to the dangling pointer example, the existence of a struct that is
+// shared across thread boundaries which shouldn't already triggers UB regardless of whether it's
+// actually accessed.
+unsafe impl Sync for StaticAnyMap {}
+
+impl StaticAnyMap {
+    fn get<T>(&'static self) -> Option<&'static T> {
+        let read_only_guard = self.inner.read();
+        let val = read_only_guard.get::<Pin<Box<T>>>()?;
+        // SAFETY:
+        // Since we only insert values into the map and we wrap the values in Pin<Box<T>> and the
+        // map itelf has a static lifetime, we can be sure that the data being pointed to has
+        // static lifetime.
+        Some(unsafe { convert_to_static_ref(val) })
+    }
+
+    fn init_and_return<T>(&'static self, init: impl FnOnce() -> T) -> &'static T {
+        let mut writeable_map = self.inner.write();
+
+        // Check again to be sure that the type wasn't inserted after we dropped the fast-path
+        // read-only lock.
+        if !writeable_map.contains::<Pin<Box<T>>>() {
+            writeable_map.insert(Box::pin(init()));
+        }
+        let read_only_guard = parking_lot::lock_api::RwLockWriteGuard::<
+            '_,
+            parking_lot::RawRwLock,
+            anymap::Map,
+        >::downgrade(writeable_map);
+        // SAFETY:
+        // Three lines above we insert the T if the map contains nothing. Since we're holding a lock
+        // the entire time, it's impossible that the map does not contain the T.
+        let val = unsafe { read_only_guard.get::<Pin<Box<T>>>().unwrap_unchecked() };
+
+        // SAFETY:
+        // Since we only insert values into the map and we wrap the values in Pin<Box<T>> and the
+        // map itelf has a static lifetime, we can be sure that the data being pointed to has
+        // static lifetime.
+        unsafe { convert_to_static_ref(val) }
+    }
+
+    pub fn get_or_init<T>(&'static self, init: impl FnOnce() -> T) -> &'static T {
+        if let Some(val) = self.get() {
+            val
+        } else {
+            self.init_and_return(init)
+        }
+    }
+}
+
+// # Safety
+// The reference MUST point to data that has a static lifetime and otherwise be a valid reference.
+unsafe fn convert_to_static_ref<T>(pin: &Pin<Box<T>>) -> &'static T {
+    let t_ref = &*pin.as_ref();
+    let ptr = t_ref as *const T;
+    // SAFETY:
+    // Check: The pointer must be properly aligned.
+    // Proof: The pointer is obtained from a valid reference so it must be aligned.
+    //
+    // Check: It must be “dereferenceable” in the sense defined in the module documentation.
+    // Proof: The pointer is obtained from a valid reference it musts be dereferenceable.
+    //
+    // Check: The pointer must point to an initialized instance of T.
+    // Proof: The AnyMap crate provides this guarantee.
+    //
+    // Check: You must enforce Rust’s aliasing rules, since the returned lifetime 'a is
+    //        arbitrarily chosen and does not necessarily reflect the actual lifetime of the data.
+    //        In particular, while this reference exists, the memory the pointer points to
+    //        must not get mutated (except inside UnsafeCell).
+    // Proof: We return a shared reference and therefore cannot be mutated unless T is
+    //        guarded with the normal rust memory protection constructs using UnsafeCell.
+    //        The data could be dropped if we ever removed it from this map however. Care
+    //        must be taken to never introduce any logic that would remove T from the map.
+    let optional_ref = unsafe { ptr.as_ref() };
+    // SAFETY:
+    // This requires the pointer not to be null. We obtained the pointer one line above
+    // from a valid reference, therefore this is considered safe to do.
+    unsafe { optional_ref.unwrap_unchecked() }
+}
